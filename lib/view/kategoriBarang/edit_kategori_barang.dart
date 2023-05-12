b93073b9f0faa53fdd1802439aacc14daa73015a
abianfikri <abronjoy46@gmail.com>
2023-05-12 21:20:28 +0700

    membuat file edit kategori barang

---
+++ lib/view/kategoriBarang/edit_kategori_barang.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EditKategoriBarang extends StatefulWidget {
+  const EditKategoriBarang({super.key});
+
+  @override
+  State<EditKategoriBarang> createState() => _EditKategoriBarangState();
+}
+
+class _EditKategoriBarangState extends State<EditKategoriBarang> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
