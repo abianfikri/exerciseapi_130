948d8921d2cc1d57addec840da744bd127279f43
abianfikri <abronjoy46@gmail.com>
2023-05-14 19:33:24 +0700

    menambahkan variable var id

--- lib/model/kategori_barang_model.dart
+++ lib/model/kategori_barang_model.dart
@@ -2,45 +2,53 @@ import 'dart:convert';
 
 class KategoriBarangModel {
   final String nama;
+  var id;
+
   KategoriBarangModel({
     required this.nama,
+    this.id,
   });
 
   KategoriBarangModel copyWith({
     String? nama,
+    int? id,
   }) {
     return KategoriBarangModel(
       nama: nama ?? this.nama,
+      id: id ?? this.id,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'nama': nama,
+      'nama_kategori_barang': nama,
+      'id_kategori_barang': id,
     };
   }
 
   factory KategoriBarangModel.fromMap(Map<String, dynamic> map) {
     return KategoriBarangModel(
-      nama: map['nama'] ?? '',
+      nama: map['nama_kategori_barang'] ?? '',
+      id: map['id_kategori_barang']?.toInt() ?? 0,
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory KategoriBarangModel.fromJson(String source) => KategoriBarangModel.fromMap(json.decode(source));
+  factory KategoriBarangModel.fromJson(String source) => KategoriBarangModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'KategoriBarangModel(nama: $nama)';
+  String toString() => 'KategoriBarangModel(nama: $nama, id: $id)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
   
     return other is KategoriBarangModel &&
-      other.nama == nama;
+      other.nama == nama &&
+      other.id == id;
   }
 
   @override
-  int get hashCode => nama.hashCode;
+  int get hashCode => nama.hashCode ^ id.hashCode;
 }
