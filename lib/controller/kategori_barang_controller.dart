8fb0ca553bcb584e8dd02de80d2d8d55b180aea1
abianfikri <abronjoy46@gmail.com>
2023-05-14 19:32:17 +0700

    menambahkan api deleteData

--- lib/controller/kategori_barang_controller.dart
+++ lib/controller/kategori_barang_controller.dart
@@ -3,36 +3,50 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:practice_api/model/kategori_barang_model.dart';
 
-class KategoriBarangController{
+class KategoriBarangController {
   final String apiUrl = "http://10.0.2.2:8000/api/";
 
-  Future<List<KategoriBarangModel>> getKategoriBarang() async{
+  Future<List<KategoriBarangModel>> getKategoriBarang() async {
     var result = await http.get(Uri.parse("${apiUrl}barang/getAllKB/"));
-    if(result.statusCode == 200){
+    if (result.statusCode == 200) {
       var data = jsonDecode(result.body); // json to map
       List<KategoriBarangModel> kategoriBarang = []; // list of kategori barang
-      for(var i in data){
+      for (var i in data) {
         // loop data
-        KategoriBarangModel kategoriModel = KategoriBarangModel.fromMap(i); // map to kategoriModel Barang
+        KategoriBarangModel kategoriModel =
+            KategoriBarangModel.fromMap(i); // map to kategoriModel Barang
         kategoriBarang.add(kategoriModel); // add kategoriBarang to list
       }
-      
+
       return kategoriBarang;
-    }else{
+    } else {
       throw Exception("Gagal mengambil data kategori barang");
     }
   }
 
-  Future addKategoriBarang(KategoriBarangModel kategoriBarang) async{
-    var result = await http.post(Uri.parse("${apiUrl}barang/addKB"), 
-    body:{
+  Future addKategoriBarang(KategoriBarangModel kategoriBarang) async {
+    var result = await http.post(Uri.parse("${apiUrl}barang/addKB"), body: {
       "nama_kategori_barang": kategoriBarang.nama,
     });
 
-    if(result.statusCode == 200){
+    if (result.statusCode == 200) {
       return jsonDecode(result.body);
-    }else{
+    } else {
       throw Exception("Gagal menambahkan data kategori barang");
+    }
+  }
+
+  Future deleteData(int id) async {
+    try {
+      var result =
+          await http.post(Uri.parse("${apiUrl}barang/deleteKB/$id"));
+      if (result.statusCode == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      print(e.toString());
+      throw Exception('Gagal menghapus data kategori barang');
     }
   }
 }
