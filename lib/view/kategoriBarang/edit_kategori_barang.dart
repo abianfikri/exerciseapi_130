08c0acf705bf142bdd26e89289b51ba7ea28b679
abianfikri <abronjoy46@gmail.com>
2023-05-14 19:34:30 +0700

    membuat halamana edit.dart

--- lib/view/kategoriBarang/edit_kategori_barang.dart
+++ lib/view/kategoriBarang/edit_kategori_barang.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,6 +12,39 @@ class EditKategoriBarang extends StatefulWidget {
 class _EditKategoriBarangState extends State<EditKategoriBarang> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var formkey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Kategori Barang'),
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nama Kategori Barang',
+                labelText: 'Nama Kategori Barang',
+              ),
+              onChanged: (value) {
+
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nama Kategori is required';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Update'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
